ec427b3ca2644ba39d92d8f0d9070c77e90c0040
Adrian-Stefan Mares <a.mares@student.tue.nl>
2019-05-06 09:46:06 +0200

    ext.keras: Update examples

--- examples/keras/openml_run_example.py
+++ examples/keras/openml_run_example.py
@@ -1,9 +1,3 @@
-"""
-OpenML Run Example
-==================
-
-An example of an automated machine learning experiment.
-"""
 import openml
 import openml.extensions.keras as keras_extension
 
@@ -11,12 +5,22 @@ import keras
 
 model = keras.models.Sequential([
     keras.layers.BatchNormalization(),
-    keras.layers.Dense(units=128, activation=keras.activations.relu),
+    keras.layers.Reshape(target_shape=(32, 32, 3)),
+    keras.layers.Conv2D(filters=32, kernel_size=[5, 5], padding='same', activation=keras.activations.relu),
+    keras.layers.MaxPool2D(pool_size=[2, 2], strides=2),
+    keras.layers.Conv2D(filters=64, kernel_size=[5, 5], padding='same', activation=keras.activations.relu),
+    keras.layers.MaxPool2D(pool_size=[2, 2], strides=2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(units=1024, activation=keras.activations.relu),
     keras.layers.Dropout(rate=0.4),
     keras.layers.Dense(units=10, activation=keras.activations.softmax),
 ])
 
-task = openml.tasks.get_task(3573)
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+task = openml.tasks.get_task(167124)
 
 run = openml.runs.run_model_on_task(model, task, avoid_duplicate_runs=False)
 run.publish()
