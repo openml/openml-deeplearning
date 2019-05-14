f8c5f5bf565a45728f6808cad3414a50c183ff98
Veselin Minev <v.minev@student.tue.nl>
2019-05-14 10:36:47 +0200

    ext.onnx: Created a simple example for fitting mxnet model created from onnx model

--- examples/onnx/mxnet_model.py
+++ examples/onnx/mxnet_model.py
@@ -1,12 +1,18 @@
-import openml.extensions.onnx
 import onnx
-import os
+import onnxmltools
+import openml
 import mxnet as mx
-import mxnet.contrib.onnx as onnx_mxnet
+import numpy as np
+import pandas as pd
+import scipy.sparse
+from mxnet.contrib.onnx import onnx2mx
+from mxnet import nd, gluon, autograd
+from keras.layers import Input, Dense, Add
+from keras.models import Model
+from openml.tasks import OpenMLClassificationTask, OpenMLRegressionTask
+from mxnet.test_utils import download
 
-############################################################################
-# Obtain task with training data
-task = openml.tasks.get_task(10101)
+task = openml.tasks.get_task(31)
 X, y = task.get_X_and_y()
 train_indices, test_indices = task.get_train_test_split_indices(
     repeat=0, fold=0, sample=0)
@@ -14,60 +20,39 @@ X_train = X[train_indices]
 y_train = y[train_indices]
 X_test = X[test_indices]
 y_test = y[test_indices]
-############################################################################
 
-############################################################################
-# Compute shapes of input and output
-output_length = len(task.class_labels)
-input_length = X_train.shape[1]
+model_url = 'https://s3.amazonaws.com/onnx-mxnet/examples/super_resolution.onnx'
+onnx_model_file = download(model_url, 'model.onnx')
 
-# Create MXNet Variables
-data = mx.sym.var('data')
-label = mx.sym.var('softmax_label')
+# TODO: Verify this works correctly
+model_mx = onnx2mx.import_to_gluon(onnx_model_file)
 
-# Create the MXNet Module API model
-bnorm = mx.sym.BatchNorm(data=data)
-fc1 = mx.sym.FullyConnected(data=bnorm, num_hidden=1024)
-act1 = mx.sym.Activation(data=fc1, act_type="relu")
-drop1 = mx.sym.Dropout(data=act1, p=0.4)
-fc2 = mx.sym.FullyConnected(data=drop1, num_hidden=output_length)
-mlp = mx.sym.SoftmaxOutput(data=fc2, name='softmax', label=label)
-mlp_model = mx.mod.Module(symbol=mlp, context=mx.cpu())
+model_mx.initialize(mx.init.Xavier(), ctx=mx.cpu())
+# Create, optimize and cache computational graph
+model_mx.hybridize()
 
-data_shapes = [('data', X_train.shape)]
-label_shapes = [('softmax_label', y_train.shape)]
+if isinstance(task, OpenMLClassificationTask):
+    loss_fn = gluon.loss.SoftmaxCrossEntropyLoss()
+elif isinstance(task, OpenMLRegressionTask):
+    loss_fn = gluon.loss.L2Loss()
 
-# Bind and initialize parameters
-mlp_model.bind(data_shapes=data_shapes, label_shapes=label_shapes)
-mlp_model.init_params(mx.init.Xavier())
+# Initialize the trainer
+# TODO: change parameters?
+trainer = gluon.Trainer(model_mx.collect_params(), 'adam',
+                        {'learning_rate': 0.1})
 
-# Save the parameters and symbol to files
-mlp_model.save_params('./model-0001.params')
-mlp.save('./model-symbol.json')
+# TODO: Need to convert to NDArrays?
+input = X_train
+labels = y_train
 
-# Export the ONNX specification of the model, using the parameters and symbol files
-onnx_mxnet.export_model(
-    sym='./model-symbol.json',
-    params='./model-0001.params',
-    input_shape=[(64, input_length)],
-    onnx_file_path='model.onnx')
-############################################################################
+# Autograd records computations done on NDArrays inside "with" block
+with autograd.record():
+    # Run forward propogation
+    # TODO: Fix data?
+    output = model_mx(input)
+    loss = loss_fn(output, labels)
 
-############################################################################
-# Load ONNX file and remove files
-model = onnx.load_model("model.onnx")
-if os.path.exists("model-0001.params"):
-    os.remove("model-0001.params")
-if os.path.exists("model-symbol.json"):
-    os.remove("model-symbol.json")
-if os.path.exists("model.onnx"):
-    os.remove("model.onnx")
-############################################################################
-# Run the Keras model on the task (requires an API key).
-run = openml.runs.run_model_on_task(model, task, avoid_duplicate_runs=False)
-# Publish the experiment on OpenML (optional, requires an API key).
-run.publish()
+loss.backward()
+trainer.step(input.shape[0])
 
-print('URL for run: %s/run/%d' % (openml.config.server, run.run_id))
-
-############################################################################
+pass
