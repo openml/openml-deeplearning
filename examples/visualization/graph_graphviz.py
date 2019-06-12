85f11325e48e783782f31706e93a19d4bb9ece7f
Veselin Minev <v.minev@student.tue.nl>
2019-06-12 09:17:15 +0200

    visualization: Improved exported graph to be more simplified

--- examples/visualization/graph_graphviz.py
+++ examples/visualization/graph_graphviz.py
@@ -13,6 +13,20 @@ OP_STYLE = {
 model = onnx.load_model('model.onnx')
 pydot_graph = GetPydotGraph(model.graph, name=model.graph.name, rankdir=".",
                             node_producer=GetOpNodeProducer(embed_docstring=True, **OP_STYLE))
+for k, v in pydot_graph.obj_dict['nodes'].items():
+    if "(op" in k:
+        orig_name = v[0]['name']
+        v[0]['name'] = orig_name.rsplit('\\n')[0] + '\"'
+for k, v in pydot_graph.obj_dict['edges'].items():
+    edge = v[0]
+    lst = []
+    for idx, rv in enumerate(edge['points']):
+        if "(op" in rv:
+            lst.append(rv.rsplit('\\n')[0] + '\"')
+        else:
+            lst.append(rv)
+    edge['points'] = lst
+    print(k, v)
 pydot_graph.write_dot("graph.dot")
 os.system('dot -Tsvg graph.dot -o graph.svg')
 
