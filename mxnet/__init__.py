2e53dee350fa7c2ed4f1ff77d3c43ce087d27362
prabhant <prabhansingh@gmaill.com>
2019-10-28 16:47:16 +0100

    init

--- mxnet/__init__.py
+++ mxnet/__init__.py
@@ -1,5 +1,5 @@
-from extension import MXNetExtension
-from config import Config
+from .extension import MXNetExtension
+from .config import Config
 from openml.extensions import register_extension
 
 __all__ = ['MXNetExtension', 'Config']
